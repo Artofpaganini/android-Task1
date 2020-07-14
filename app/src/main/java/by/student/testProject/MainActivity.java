13497bf3f5f68a02bb84a51a208f339edb9e4aee
victor <Artofpaganini@gmail.com>
2020-07-14 09:14:31 +0300

    fix  bags

--- app/src/main/java/by/student/testProject/MainActivity.java
+++ app/src/main/java/by/student/testProject/MainActivity.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
